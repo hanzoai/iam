2734feba2ad0d35972a74c9c5a3b1a33e78fa1e2
Zach Kelling <z@zeekay.io>
2026-02-20 22:58:14 -0800

    refactor: replace all casdoor imports with hanzoid equivalents
    Rewrite all github.com/casdoor/* imports to github.com/hanzoid/*
    across 45 Go source files. Remove replace directives from go.mod
    since we now import hanzoid packages directly.

--- deployment/deploy.go
+++ deployment/deploy.go
@@ -20,7 +20,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/casdoor/oss"
+	"github.com/hanzoid/oss"
 	"github.com/hanzoai/iam/object"
 	"github.com/hanzoai/iam/storage"
 	"github.com/hanzoai/iam/util"
