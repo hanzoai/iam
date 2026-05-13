972296a16b45c6d679f84cc86a7f961df9c7e715
Hanzo Dev <dev@hanzo.ai>
2026-05-12 23:19:49 -0700

    fmt: gofumpt-format notification, ldap, storage after hanzoai/* import rename

--- notification/line.go
+++ notification/line.go
@@ -15,9 +15,9 @@
 package notification
 
 import (
+	"github.com/hanzoai/iam/proxy"
 	notify "github.com/hanzoai/notify2"
 	"github.com/hanzoai/notify2/service/line"
-	"github.com/hanzoai/iam/proxy"
 )
 
 func NewLineProvider(channelSecret string, accessToken string, receiver string) (*notify.Notify, error) {
