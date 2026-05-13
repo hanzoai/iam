d1b8c06ad85065e2de89c324634c3ee90d64d4fb
Zach Kelling <z@zeekay.io>
2026-05-12 23:19:49 -0700

    fmt: gofumpt-format notification, ldap, storage after hanzoai/* import rename

--- notification/telegram.go
+++ notification/telegram.go
@@ -17,10 +17,10 @@ package notification
 import (
 	"strconv"
 
-	notify "github.com/hanzoai/notify2"
-	"github.com/hanzoai/notify2/service/telegram"
 	api "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/hanzoai/iam/proxy"
+	notify "github.com/hanzoai/notify2"
+	"github.com/hanzoai/notify2/service/telegram"
 )
 
 func NewTelegramProvider(apiToken string, chatIdStr string) (notify.Notifier, error) {
