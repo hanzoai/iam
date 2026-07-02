cb835a1101bf6a5bfb8799f709eabe0eb98a87d5
Zach Kelling <z@zeekay.io>
2026-07-02 12:14:07 -0700

    chore: restore Casdoor copyright headers in forked source (Apache-2.0 §4c) + assert Hanzo modifications

--- controllers/enforcer.go
+++ controllers/enforcer.go
@@ -1,4 +1,5 @@
-// Copyright 2023 The Hanzo Authors. All Rights Reserved.
+// Copyright 2023 The Casdoor Authors. All Rights Reserved.
+// Modifications Copyright 2025 Hanzo AI, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- object/syncer_user_test.go
+++ object/syncer_user_test.go
@@ -1,4 +1,5 @@
-// Copyright 2021 The Hanzo Authors. All Rights Reserved.
+// Copyright 2021 The Casdoor Authors. All Rights Reserved.
+// Modifications Copyright 2025 Hanzo AI, Inc.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
