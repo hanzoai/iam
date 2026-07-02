7bd00de2b0c2a21f7c29a7e9867cbe7e8083c582
Zach Kelling <z@zeekay.io>
2026-07-02 12:30:07 -0700

    chore: correct upstream attribution on 4 remaining forked files (§4c)
    The prior rebrand left 4 forked files with a 'Hanzo AI Inc' header instead of the
    upstream party: util/system.go + routers/lightweight_auth_filter.go are Casdoor-origin;
    util/string_test.go + object/user_webauthn.go are casbin-origin. Restore the correct
    'The <Casdoor|casbin> Authors' copyright + Modifications Copyright Hanzo line. Completes
    Apache-2.0 §4(c) coverage across the fork.

--- routers/lightweight_auth_filter.go
+++ routers/lightweight_auth_filter.go
@@ -1,4 +1,5 @@
-// Copyright 2026 Hanzo AI Inc. Apache 2.0 licensed.
+// Copyright 2026 The Casdoor Authors. All Rights Reserved.
+// Modifications Copyright 2025 Hanzo AI, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
