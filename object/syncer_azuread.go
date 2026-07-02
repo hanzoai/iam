fd0e4572bac8b975f0be582a996893d62e35c0df
Hanzo Dev <dev@hanzo.ai>
2026-07-02 12:14:07 -0700

    chore: restore Casdoor copyright headers in forked source (Apache-2.0 §4c) + assert Hanzo modifications

--- object/syncer_azuread.go
+++ object/syncer_azuread.go
@@ -1,4 +1,5 @@
-// Copyright 2025 The Hanzo Authors. All Rights Reserved.
+// Copyright 2025 The Casdoor Authors. All Rights Reserved.
+// Modifications Copyright 2025 Hanzo AI, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
